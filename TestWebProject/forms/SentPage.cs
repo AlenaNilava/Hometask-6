d8414434d1dc96ee54590736f89832e3846da190
Alena Nilava <44751670+AlenaNilava@users.noreply.github.com>
2019-01-29 23:41:25 +0300

    Added test using Page Object Pattern

---
+++ TestWebProject/forms/SentPage.cs
@@ -0,0 +1,30 @@
+﻿using OpenQA.Selenium;
+using System;
+
+namespace TestWebProject.forms
+{
+    using TestWebProject.wibdriver;
+    class SentPage : BaseForm
+    {
+        private static readonly By SentLbl = By.XPath("//span[text()='Отправленные']");
+
+        public SentPage() : base(SentLbl, "Sent Page")
+        {
+        }
+
+        static string sentEmailLblTemplate = "(//a[@data-subject='{0}'])[1]";
+        readonly string sentEmailCheckboxTemplate = $"{sentEmailLblTemplate}//div[@class='b-checkbox__box']";
+        private readonly BaseElement DeleteBtn = new BaseElement(By.XPath("(//div[@data-cache-key='500000_undefined_false']//span[text()='Удалить'])[1]"));
+
+        public void CheckSentEmail(string subject)
+        {
+            new BaseElement(By.XPath(String.Format(sentEmailLblTemplate, subject))).WaitForIsVisible();
+        }
+
+        public void DeleteEmail(string subject)
+        {
+            new BaseElement(By.XPath(String.Format(sentEmailCheckboxTemplate, subject))).Click();
+            DeleteBtn.JsClick();
+        }
+    }
+}
